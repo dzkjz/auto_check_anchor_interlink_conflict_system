e58ea18e23cbe2b38e958d0bf68f0d51b5496d4e
dzkjz <dzkjzdzkjz@gmail.com>
2022-06-18 12:15:37 +0800

    补全逻辑

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from news_page_crawler import crawler
+import check_anchors
 
 c = crawler()
-# c.continuous_start(headless=True)
-
+# 抓取news详情页URL
+c.continuous_start(headless=True)
+# 抓取news下详情页面源码
 c.pull_source_code()
+
+# 提取详情页内容文本
+check_anchors.start(no_save_to_database=False)
+
+# 分析冲突细节
+check_anchors.conflict_filter(no_save_to_database=False)
